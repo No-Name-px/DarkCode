8591805443912bc76c9531f480381487a3593f50
Kirill <faceless369@yandex.ru>
2020-01-01 11:57:38 +0300

    L1
    Пофиксил баг с изменением ширины экрана

--- L1 Responsive NavBar/script.js
+++ L1 Responsive NavBar/script.js
@@ -1,3 +1,12 @@
 $(".icon").on("click", function () {
 	$("ul").slideToggle();
 });
+
+$(window).resize(function () {
+	if ($(window).width() > 550) {
+		$("ul").slideDown(0);
+	}
+	else {
+		$("ul").slideUp(0);
+	}
+});
