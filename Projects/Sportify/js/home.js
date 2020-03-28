badc083d4c7dfe6a9a98ea03677c8e48db956753
Kirill <kkorunyak@mail.ru>
2020-03-28 19:26:10 +0300

    Sportify
    added slider plugin

--- Projects/Sportify/js/home.js
+++ Projects/Sportify/js/home.js
@@ -1,81 +1,27 @@
 $(function() {
-  // Init Slider Intro =========
-  initInfSlider("intro", 4000, 1);
+  // Init Slider Intro ========
+  initHomeSlider();
 
-  // Init Slider ==================================
-
-  function initInfSlider(slider, interval, slidesPerPage) {
-    let sliderName = "." + slider + "-slider";
-    let slidesWrapper = $(sliderName + " .slides-wrapper");
-    let slides = $(sliderName + " .slide");
-
-    let activeNumber = 0;
-
-    let controllerPoints = $(
-      sliderName + " .slider-controller .controller-item"
-    );
-
-    for (let i = 0; i < slides.length; i++) {
-      if (i <= slidesPerPage) {
-        $(slides[i]).addClass("active");
-      }
-    }
-
-    setInterval(function changeSlide() {
-      if (controllerPoints) {
-        for (let i = 0; i < slidesPerPage; i++) {
-          $(controllerPoints[(activeNumber + i) % slides.length]).removeClass(
-            "active"
-          );
-          $(slides[(activeNumber + i) % slides.length]).removeClass("active");
-        }
-
-        if (activeNumber >= slides.length - 1 - (slidesPerPage - 1)) {
-          activeNumber = 0;
-        } else {
-          activeNumber++;
-        }
-
-        for (let i = 0; i < slidesPerPage; i++) {
-          $(controllerPoints[(activeNumber + i) % slides.length]).addClass(
-            "active"
-          );
-          $(slides[(activeNumber + i) % slides.length]).addClass("active");
-        }
-      } else {
-        $(slides[activeNumber]).removeClass("active");
-
-        if (activeNumber >= slides.length - 1) {
-          activeNumber = 0;
-        } else {
-          activeNumber++;
-        }
-
-        $(slides[activeNumber]).addClass("active");
-      }
-
-      slidesWrapper.css(
-        "transform",
-        `translateX(${activeNumber * -(100 / slidesPerPage)}%)`
-      );
-    }, interval);
-
-    if (controllerPoints) {
-      $(controllerPoints).on("click", function() {
-        for (let i = 0; i < controllerPoints.length; i++) {
-          if (controllerPoints[i] === this) {
-            $(controllerPoints[activeNumber]).removeClass("active");
-            $(slides[activeNumber]).removeClass("active");
-            activeNumber = i;
-            $(controllerPoints[activeNumber]).addClass("active");
-            $(slides[activeNumber]).addClass("active");
-            slidesWrapper.css(
-              "transform",
-              `translateX(${activeNumber * -100}%)`
-            );
-          }
-        }
+  function initHomeSlider() {
+    if ($(".intro-slider").length) {
+      var homeSlider = $(".intro-slider");
+      // var homeContent = $('.home_content');
+      homeSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1200,
+        nav: false,
+        dots: true
       });
+
+      // homeSlider.on('changed.owl.carousel', function(event)
+      // {
+      // 	homeContent.removeClass('active');
+      // 	var currentItem = $('.slide', homeSlider).eq(event.item.index);
+      // 	var asd = currentItem.find('.home_content');
+      // 	asd.addClass('active');
+      // });
     }
   }
 });
